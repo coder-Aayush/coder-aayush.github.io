3b559a4db68d772e1279fb42990dca069507a826
coder-Aayush <bhattaraiaayush08@gmail.com>
2021-03-16 23:41:50 +0545

    About View

--- lib/screen/view/about_view.dart
+++ lib/screen/view/about_view.dart
@@ -1,3 +1,4 @@
+import 'package:coderaayush/widgets/title_view.dart';
 import 'package:flutter/material.dart';
 
 class AboutView extends StatelessWidget {
@@ -16,28 +17,27 @@ class AboutView extends StatelessWidget {
             child: Column(
               children: [
                 SizedBox(height: 20),
-                Text(
-                  'About Me',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 40,
-                  ),
-                ),
-                SizedBox(height: 10),
+                TitleHeader(header: 'About Me'),
+                SizedBox(height: 2),
                 Align(
                   alignment: Alignment.topLeft,
                   child: SelectableText(
                     'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s. ',
-                    style: TextStyle(
-                      fontSize: 14,
-                    ),
                   ),
                 ),
                 SizedBox(height: 14),
                 Text(
-                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'),
+                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+                ),
                 SizedBox(height: 14),
-                ElevatedButton(onPressed: () {}, child: Text('Get Width')),
+                ElevatedButton(onPressed: () {}, child: Text('Hire Me')),
+                SizedBox(height: 24),
+                TitleHeader(header: 'Skills'),
+                SizedBox(height: 10),
+                // TODO: Add circular Progress indicator for representation of skills
+                // TODO: Migrate the package to sound null safety
+                TitleHeader(header: 'Journey'),
+                // TODO: Add the full timeline of my journey
               ],
             ),
           ),
---
+++ lib/widgets/title_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class TitleHeader extends StatelessWidget {
+  final String? header;
+  const TitleHeader({@required this.header});
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: Text(
+        header!,
+        style: TextStyle(
+          fontWeight: FontWeight.w600,
+          fontSize: 24,
+        ),
+      ),
+    );
+  }
+}
