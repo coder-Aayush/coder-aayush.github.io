5c5ccd13631e19bb28f74d945d8f73b1637648a5
coder-Aayush <bhattaraiaayush08@gmail.com>
2021-04-05 11:36:41 +0545

    Refactor Code

--- lib/screen/view/project_view.dart
+++ lib/screen/view/project_view.dart
@@ -1,3 +1,4 @@
+import 'package:coderaayush/helpers/flutter_hamlet.dart';
 import 'package:flutter/material.dart';
 import 'package:line_icons/line_icon.dart';
 import 'package:line_icons/line_icons.dart';
@@ -8,13 +9,7 @@ class ProjectView extends StatelessWidget {
     return Center(
         child: ConstrainedBox(
       constraints: BoxConstraints(maxWidth: 1080),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            BlogCard(),
-          ],
-        ),
-      ),
+      child: BlogCard(),
     ));
   }
 }
@@ -22,34 +17,32 @@ class ProjectView extends StatelessWidget {
 class BlogCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return return Card(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      IconButton(icon: Icon(LineIcons.link), onPressed: () {}),
-                      IconButton(
-                          icon: Icon(LineIcons.github), onPressed: () {}),
-                      IconButton(
-                          icon: Icon(LineIcons.googlePlay), onPressed: () {}),
-                    ],
-                  ),
-                  Text('Project Title Goes Here'),
-                  Text('Project Discription goes here...'),
-                  Wrap(
-                    children: [
-                      TextButton(onPressed: null, child: Text('Python')),
-                      TextButton(onPressed: null, child: Text('Python')),
-                    ],
-                  ),
-                ],
-              ),
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(icon: Icon(LineIcons.link), onPressed: () {}),
+                IconButton(icon: Icon(LineIcons.github), onPressed: () {}),
+                IconButton(icon: Icon(LineIcons.googlePlay), onPressed: () {}),
+              ],
             ),
-          );
+            Text('Project Title Goes Here'),
+            Text('Project Discription goes here...'),
+            Wrap(
+              children: [
+                TextButton(onPressed: null, child: Text('Python')),
+                TextButton(onPressed: null, child: Text('Python')),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
